1bc0f75fbdd4098cd88df6f9a6ccaf0faed84dd3
The Hao <nt.hao@tego.global>
2024-08-31 18:14:11 +0700

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { UsersModule } from './modules/users/users.module';
+// import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+  //  UsersModule,
+  //   MailerModule.forRoot({
+  //     transport: {
+  //       host: process.env.EMAIL_HOST,
+  //       port: process.env.EMAIL_PORT,
+  //       secure: false,
+  //       auth : {
+  //         user: process.env.EMAIL_USER,
+  //         pass: process.env.EMAIL_PASS,
+  //       },
+  //     },
+  //     defaults: {
+  //       from: `"No Reply" <no-reply@gmail.com>`,
+  //     },
+  //   })
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
